15b9e6252e70c257ee59a62e66d5202379b20071
Yaseenmogb <yaseenmogp@gmail.com>
2026-06-18 14:29:21 +0300

    Add Vercel serverless setup

---
+++ api/index.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import express from "express";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
+import { appRouter } from "../server/routers";
+import { createContext } from "../server/_core/context";
+import { registerAdminAuthRoutes } from "../server/adminAuth";
+
+const app = express();
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+registerAdminAuthRoutes(app);
+app.use("/api/trpc", createExpressMiddleware({ router: appRouter, createContext }));
+
+export default app;
